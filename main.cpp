96e05953d9a6c816c2b96bbe50f3402ebc5a5a1d
PavelMesh <meshpv1@mail.ru>
2026-06-28 16:10:22 +0300

    Commit 2

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@ int main() {
   std::cout <<"Htllo World"<<std::endl;
   std::cout << "A new commit of the change so mine.\n";
   std::cout <<"A new 'Commit 2' of the change so mine.\n";
-  std::cout <<"A new 'Commit 3' of the change so mine.\n";
+  
+  std::cout << "Ночь, улица, фонарь, аптека,\n";
     return 0;
 }
 /*
-  
+std::cout <<"A new 'Commit 3' of the change so mine.\n";
+
+std::cout << "Бессмысленный и тусклый свет.\n";
+std::cout << "Живи ещё хоть четверть века —\n";
+std::cout << "Всё будет так. Исхода нет.\n";
   */
