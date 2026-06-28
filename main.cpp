1fd06e8e5c8755a5222a6736ff4e7aed9ca80678
PavelMesh <meshpv1@mail.ru>
2026-06-28 08:12:28 +0300

    comit 1

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main() {
-  std::cout <<"Htllo World\n"<<std::endl;
-  std::cout << "A new commit of the changesomine.\n";
-  std::cout <<"A new Commit 2 of the changesomine.\n";
-  
+  std::cout <<"Htllo World"<<std::endl;
+  std::cout << "A new commit of the change so mine.\n";
+  std::cout <<"A new 'Commit 2' of the change so mine.\n";
+  std::cout <<"A new 'Commit 3' of the change so mine.\n";
     return 0;
 }
 /*
-  std::cout <<"A new Commit 3 of the changesomine.\n";
+  
   */
