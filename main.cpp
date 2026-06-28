ae7bb9053d602c0321e1396d5e6a9e719b0a0fa4
PavelMesh <meshpv1@mail.ru>
2026-06-28 20:11:44 +0300

    third dev commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-  std::cout <<"Htllo World"<<std::endl;
+  std::cout <<"Hello World"<<std::endl;
   std::cout << "A new commit of the change so mine.\n";
   std::cout <<"A new 'Commit 2' of the change so mine.\n";
   
